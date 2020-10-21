60745d1982485cf7c006168ddb5865f3eb39cd97
Cristian <crisal1395@gmail.com>
2020-10-21 17:03:41 -0600

    build

---
+++ project2/build/precache-manifest.e8ff3ccde5b8f35dd8cef86dcca1b7b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f13cabd74b6ea7b9a081b665115d211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec17ee64b2ff455d9c04",
+    "url": "/static/css/main.5e7257a6.chunk.css"
+  },
+  {
+    "revision": "9de440cc3e40f22b8cb8",
+    "url": "/static/js/2.e2f0c560.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.e2f0c560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec17ee64b2ff455d9c04",
+    "url": "/static/js/main.09aa7d5b.chunk.js"
+  },
+  {
+    "revision": "a11f9b6500f3954c2927",
+    "url": "/static/js/runtime-main.100f9daa.js"
+  },
+  {
+    "revision": "f03e96f120ce519380aa2e6a4c81f6ea",
+    "url": "/static/media/back.f03e96f1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
